0267ffa0da47bbbae1724d8b69611692f5de4f53
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-02-16 17:49:28 -0300

    feat(server): add Socket instance

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
 import { routes } from 'routes';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import { SocketConnection } from 'sockets';
 
 const PORT = 3333;
 
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
+SocketConnection(io);
+
 // eslint-disable-next-line no-console
-app.listen(PORT, () => console.log(`Running at port ${PORT}!`));
+io.listen(app.listen(PORT, () => console.log(`Running at port ${PORT}!`)));
