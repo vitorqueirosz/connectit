0d88f28f36487d1994cba8d3dd732ce9673cbb3a
vitorqueirosz <vitor.queiroz16@hotmail.com>
2022-02-21 17:42:10 -0300

    feat(AuthRepository): add accessSpotifyToken method

--- src/repositories/AuthenticationRepository.ts
+++ src/repositories/AuthenticationRepository.ts
@@ -1,7 +1,13 @@
-import request from 'request';
 import { PrismaClient, User } from '@prisma/client';
 import { UserService, AuthenticationService } from 'services';
 import { SetSpotifyTokens } from 'interfaces/User';
+import qs from 'qs';
+import axios from 'axios';
+import { SPOTIFY_BASE_URL } from 'controllers/AuthenticationController';
+import {
+  SpotifyAccessTokenPayload,
+  SpotifyTokenPayload,
+} from 'interfaces/Spotify';
 
 interface AuthenticationResponse {
   user: User;
@@ -18,6 +24,11 @@ interface IAuthenticationRepository {
   ) => Promise<AuthenticationResponse>;
 }
 
+interface AccessSpotifyTokenPayload {
+  code: string;
+  redirect_uri: string;
+  state: number;
+}
 export class AuthenticationRepository implements IAuthenticationRepository {
   constructor(private prisma: PrismaClient) {
     this.prisma = prisma;
@@ -76,6 +87,38 @@ export class AuthenticationRepository implements IAuthenticationRepository {
     return userExists.access_spotify_token;
   }
 
+  async accessSpotifyToken({
+    code,
+    redirect_uri,
+    state,
+  }: AccessSpotifyTokenPayload) {
+    const payload = {
+      code,
+      redirect_uri,
+      grant_type: 'authorization_code',
+    };
+
+    const { data } = await axios.post<SpotifyAccessTokenPayload>(
+      `${SPOTIFY_BASE_URL}/api/token`,
+      qs.stringify(payload),
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`,
+          ).toString('base64')}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    await this.setSpotifyTokens({
+      user_id: Number(state),
+      access_token: data.access_token,
+      refresh_token: data.refresh_token,
+    });
+  }
+
   async refreshSpotifyToken(user_id: number) {
     const userService = new UserService(this.prisma);
 
@@ -83,32 +126,34 @@ export class AuthenticationRepository implements IAuthenticationRepository {
 
     if (!user) throw new Error('User not found');
 
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      headers: {
-        Authorization: `Basic ${Buffer.from(
-          `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`,
-        ).toString('base64')}`,
-      },
-      form: {
-        grant_type: 'refresh_token',
-        refresh_token: user.refresh_spotify_token,
-      },
-      json: true,
+    const payload = {
+      grant_type: 'refresh_token',
+      refresh_token: user.refresh_spotify_token,
     };
 
-    request.post(authOptions, async (error, reponse, body) => {
-      const { access_token, refresh_token } = body;
+    const { data } = await axios.post<SpotifyTokenPayload>(
+      `${SPOTIFY_BASE_URL}/api/token`,
+      qs.stringify(payload),
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(
+            `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`,
+          ).toString('base64')}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
 
-      await this.prisma.user.update({
-        where: {
-          id: user_id,
-        },
-        data: {
-          access_spotify_token: access_token,
-          refresh_spotify_token: refresh_token,
-        },
-      });
+    await this.prisma.user.update({
+      where: {
+        id: user_id,
+      },
+      data: {
+        access_spotify_token: data.access_token,
+      },
     });
+
+    return data.access_token;
   }
 }
